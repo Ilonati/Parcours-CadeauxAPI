6ff45e1fa5b4f94c510ff1c8628f9db9999ef09b
Ilona Tykhonovskaya <ilonatiho@gmail.com>
2025-07-26 23:41:06 +0200

    1

--- routes/gifts.routes.js
+++ routes/gifts.routes.js
@@ -6,8 +6,18 @@ const giftSchema = require('../schemas/gift.schema');
 // Liste des cadeaux
 router.get('/', async (req, res, next) => {
     try {
+        let sql = "SELECT * FROM gifts";
+        const params = [];
+
+        if (req.query.reserved === "true") {
+            sql += " WHERE reserved = true";
+        }
+
+        if (req.query.sort === "asc" || req.query.sort === 'desc') {
+            sql += ` ORDER BY price ${req.query.sort}`;
+        }
         //TODO : Ecrire la requête dans les '' qui permet de récuperer tous les cadeaux
-        const [gifts] = await db.query('');
+        const [gifts] = await db.query(sql, params);
         res.json(gifts);
     } catch (err) {
         next(err);
@@ -19,11 +29,11 @@ router.post('/', async (req, res, next) => {
     try {
         const { error, value } = giftSchema.validate(req.body);
         if (error) return res.status(400).json({ error: error.details[0].message });
-        
+
         const { title, description, price, reserved = false } = value;
         //TODO : Ecrire la requête dans les '' qui permet d'ajouter un cadeau sur SQL
         const [result] = await db.query(
-            '',
+            'INSERT INTO gifts (title, description, price, reserved) VALUES (?, ?, ?, ?)',
             [title, description, price, reserved]
         );
         res.status(201).json({ id: result.insertId, ...value });
@@ -38,10 +48,24 @@ router.patch('/:id', async (req, res, next) => {
         const { error, value } = giftSchema.validate(req.body, { presence: 'optional' });
         if (error) return res.status(400).json({ error: error.details[0].message });
 
-                //TODO : Ecrire la requête dans les '' qui permet de modifier un cadeau sur SQL
-        const [result] = await db.query('', [value, req.params.id]);
+        //TODO : Ecrire la requête dans les '' qui permet de modifier un cadeau sur SQL
+        const [result] = await db.query('UPDATE gifts SET ? WHERE id = ?', [value, req.params.id]);
         if (result.affectedRows === 0) return res.status(404).json({ error: 'Cadeau non trouvé' });
         res.json({ message: 'Cadeau mis à jour' });
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Marquer comme réservé
+router.patch('/:id/reserve', async (req, res, next) => {
+    try {
+        const giftId = req.params.id;
+        const sql = 'UPDATE gifts SET reserved = true WHERE id = ?';
+        const [result] = await db.query(sql, [giftId]);
+
+        if (result.affectedRows === 0) return res.status(404).json({ error: "Cadeau non trouvé" });
+        res.json({ message: `Cadeau ${giftId} marqué comme réservé` });
     } catch (err) {
         next(err);
     }
