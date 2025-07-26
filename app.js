4ca451de382aabe8ed7283ea2d59abee814e40a4
Ilona Tykhonovskaya <ilonatiho@gmail.com>
2025-07-27 00:41:26 +0200

    capts

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ const app = express();
     }
 })();
 
+
+
+
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+
 //TODO : Initaliser la database
 const db = mysql.createPool({
     host: process.env.DB_HOST,
